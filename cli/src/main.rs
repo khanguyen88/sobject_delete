b682320b4914e9b5c0f60ef18ce33b8907a2c8e9
Kha Nguyen <darthhanter@gmail.com>
2019-05-16 16:09:08 +0700

    add cli arg

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,22 @@
+#[macro_use]
+extern crate structopt;
+
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(rename_all = "kebab-case")]
+struct CliArg {
+    #[structopt(short, long)]
+    debug: bool,
+    #[structopt(parse(from_os_str), value_delimiter = ";")]
+    paths: Vec<PathBuf>,
+    #[structopt(short = "a", long)]
+    apex_template: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CliArg::from_args();
+    println!("{:#?}", args);
 }
